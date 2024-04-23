f8c503e52f3e9ac5f197e7084587c65aafb10ffc
DanielaOrd97 <201G0272@rcarbonifera.tecnm.mx>
2024-04-23 17:31:54 -0600

    Metodos Put y Delete

--- ITESRCLibros/ITESRCLibrosAPI/Controllers/LibrosController.cs
+++ ITESRCLibros/ITESRCLibrosAPI/Controllers/LibrosController.cs
@@ -42,7 +42,7 @@ namespace ITESRCLibrosAPI.Controllers
                 {
                     Id = 0,
                     Eliminado = false,
-                    FechaActualizacion = DateTime.Now,
+                    FechaActualizacion = DateTime.UtcNow,
                     Autor = dto.Autor,    //MAPEO
                     Portada = dto.Portada, //MAPEO
                     Titulo = dto.Titulo //MAPEO
@@ -75,6 +75,64 @@ namespace ITESRCLibrosAPI.Controllers
                 });
 
             return Ok(libros);
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult Put(LibroDTO dto)
+        {
+            LibroValidator validator = new();
+            var resultados = validator.Validate(dto);
+
+            //2.- Si es valido, agregar.
+            if (resultados.IsValid)
+            {
+                //Buscar la entidad del dto
+                var entidadLibro = Repository.Get(dto.Id ?? 0);
+
+                //verificar si se encontro (encontrado, no encontrado, encontrado pero esta borrado (baja logica))
+                if(entidadLibro == null || entidadLibro.Eliminado == true)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    entidadLibro.Autor = dto.Autor;
+                    entidadLibro.Titulo = dto.Titulo;
+                    entidadLibro.Portada = dto.Portada;
+                    entidadLibro.FechaActualizacion = DateTime.UtcNow;
+
+                    Repository.Update(entidadLibro);
+
+                    return Ok();
+
+                }
+
+                
+            }
+
+            return BadRequest(resultados.Errors.Select(x => x.ErrorMessage));
+
+
+
+        }
+
+
+        [HttpDelete ("{id}")] //No acepta Dtos, solo ids
+        public IActionResult Delete(int id)
+        {
+            var entidadLibro = Repository.Get(id);
+
+            if(entidadLibro == null || entidadLibro.Eliminado)
+            {
+                return NotFound();
+            }
+
+            entidadLibro.Eliminado = true;
+            entidadLibro.FechaActualizacion = DateTime.UtcNow;
+            Repository.Update(entidadLibro);
+
+            return Ok();
         }
     }
 }
