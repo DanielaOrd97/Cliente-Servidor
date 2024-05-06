74e05c5159311b9d84adb77887e791ffbf316dd0
DanielaOrd97 <201G0272@rcarbonifera.tecnm.mx>
2024-05-06 17:52:30 -0600

    ...

---
+++ PruebaJWT1/Controllers/SaludosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaJWT1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SaludosController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok("Hola a todos");
+        }
+    }
+}
